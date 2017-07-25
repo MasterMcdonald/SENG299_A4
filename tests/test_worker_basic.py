79e221996c71a846bca8ced8168b0311b0a51b5e
MasterMcdonald <zelanxiang@gmail.com>
2017-07-25 15:16:01 -0700

    add first test

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -61,15 +61,14 @@ class TestWorkerBasic(unittest.TestCase):
         self.assertEqual(len_to_crawl_after, len_to_crawl_before)
     """
     def test_zelan_test_one(self):
-        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
+        worker = BasicUserParseWorker("https://www.reddit.com")
         worker.crawled = []
 
-        worker.add_links(["https://www.reddit.com/user/Chrikelnel"])
         len_to_crawl_before = len(worker.to_crawl)
         worker.add_links(["https://www.reddit.com/user/Chrikelnel"])
         len_to_crawl_after = len(worker.to_crawl)
 
-        self.assertEqual(len_to_crawl_after, len_to_crawl_before)
+        self.assertEqual(len_to_crawl_after, len_to_crawl_before + 1)
 
 
 
