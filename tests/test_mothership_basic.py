335628b3813cf180a4d5940b3f25882ec46c6d34
MasterMcdonald <zelanxiang@gmail.com>
2017-07-26 03:05:12 -0700

    add second test

--- tests/test_mothership_basic.py
+++ tests/test_mothership_basic.py
@@ -2,10 +2,11 @@
 import unittest
 
 from mothership.base import MothershipServer
+from workers.basic_worker import BasicUserParseWorker
 
 
 class TestMothershipBasic(unittest.TestCase):
-    pass
+
     def test_zelan_test_three(self):
         """
         this test is to test if handle worker contact
@@ -14,5 +15,8 @@ class TestMothershipBasic(unittest.TestCase):
         mothership = MothershipServer()
         mothership.run()
         worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
-        worker.run()
-        self.assertRaises(ValueError, worker.send_to_mother(None, mothership))
+        try:
+            worker.run()
+        except ConnectionRefusedError:
+            self.fail("Error")
+        #self.assertRaises(ValueError, worker.send_to_mother(None, mothership))
