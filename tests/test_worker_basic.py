953d34707ace95890c570c4762671fc8f2cde3e7
MasterMcdonald <zelanxiang@gmail.com>
2017-07-26 02:26:19 -0700

    add second test

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -74,7 +74,20 @@ class TestWorkerBasic(unittest.TestCase):
 
         self.assertEqual(len_to_crawl_after, len_to_crawl_before + 1)
 
+    def test_zelan_test_two(self):
+        """
+        this test is to test if handle worker contact
+        worker not running
+        """
+        worker = None
+        worker = BasicUserParseWorker("https://www.reddit.com")
+        worker.crawled = []
 
+        len_to_crawl_before = len(worker.to_crawl)
+        worker.add_links(["https://www.reddit.com/user/Chrikelnel"])
+
+        self.assertIn("https://www.reddit.com/user/Chrikelnel", worker.to_crawl)
+    
         
 
 
